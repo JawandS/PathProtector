7deef2aed5b076475e175c8394aa007bd9989b86
Jawand <jawandsingh@gmail.com>
2023-03-04 15:19:42 -0500

    attempt accelerometer test

--- app/src/main/java/com/singhjawand/pathprotector/MainActivity.java
+++ app/src/main/java/com/singhjawand/pathprotector/MainActivity.java
@@ -1,14 +1,31 @@
 package com.singhjawand.pathprotector;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    double ax, ay, az;   // acceleration in x, y, z
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            ax = event.values[0];
+            ay = event.values[1];
+            az = event.values[2];
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Delete entry")
+                    .setMessage("Are you sure you want to delete this entry?");
+        }
+    }
+
 }
